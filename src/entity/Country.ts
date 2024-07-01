008f384540dfdb55619c19e9499a42835d66db71
Robin <robin.kolasinski@gmail.com>
2024-07-01 09:38:40 +0200

    initialized Country.ts

---
+++ src/entity/Country.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class Country {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  code: string;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column()
+  emoji: string;
+
+  @Field()
+  @Column()
+  continent: string;
+}
